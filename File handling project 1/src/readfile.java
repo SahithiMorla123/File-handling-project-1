e8c68453fad9265b961e458d6c3647c217400214
admin <admin@public1.114dns.com>
2022-07-25 23:12:31 +0530

    file handling

---
+++ File handling project 1/src/readfile.java
@@ -0,0 +1,20 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class readfile {
+	
+	public static void main(String[] args) {
+		char[] data = new char[100];
+		
+		try {
+			FileReader input = new FileReader("FileHandling.txt");
+			input.read(data);
+			System.out.println("File Data Read Successfully");
+			System.out.println(data);
+			input.close();
+		} catch (IOException e) {
+			System.out.println("File not found");
+			e.printStackTrace();
+		}
+	}
+}
